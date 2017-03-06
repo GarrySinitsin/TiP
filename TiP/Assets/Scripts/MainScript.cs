7dc3b0f6caae32e6120d173cb735226a537a03fa
ZAGRAVA\kicum <ihor.sinitsyn@zagravagames.com>
2017-03-06 11:46:34 +0200

    Initial commat

---
+++ TiP/Assets/Scripts/MainScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainScript : MonoBehaviour {
+
+	public void StartButOnclick(){
+		SceneManager.LoadScene (2);
+	}
+	public void QuitButOnclick(){
+		Application.Quit();
+	}
+	public void optionsButOnclick(){
+		SceneManager.LoadScene (1);
+	}
+
+}
